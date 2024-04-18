bc9ed13d39401ba571eb575814f78944eaa0e92d
Christian Oudard <ChristianOudard@pm.me>
2024-04-17 23:47:47 -0600

    Display node degrees. Check doubled edges.

--- graph_layout.py
+++ graph_layout.py
@@ -1,14 +1,15 @@
+import sys
 import networkx as nx
 import matplotlib.pyplot as plt
 
 # Read the DOT file
-graph = nx.drawing.nx_pydot.read_dot('output.dot')
+graph = nx.drawing.nx_pydot.read_dot(sys.argv[1])
 
 # Apply a layout algorithm (e.g., spring layout)
 pos = nx.spring_layout(graph)
 
 # Draw the nodes
-nx.draw_networkx_nodes(graph, pos, node_size=500, node_color='lightblue')
+nx.draw_networkx_nodes(graph, pos, node_size=800, node_color='lightblue')
 
 # Get the edge styles and colors
 edge_styles = nx.get_edge_attributes(graph, 'style')
@@ -21,8 +22,10 @@ nx.draw_networkx_edges(graph, pos, edge_color=edge_colors, style=edge_styles.val
 edge_labels = nx.get_edge_attributes(graph, 'label')
 nx.draw_networkx_edge_labels(graph, pos, edge_labels=edge_labels)
 
-# Draw the node labels
-nx.draw_networkx_labels(graph, pos, font_size=12, font_weight='bold')
+# Draw the node labels, strip quotes.
+node_labels = nx.get_node_attributes(graph, 'label')
+node_labels = {node: label.strip('"') for node, label in node_labels.items()}
+nx.draw_networkx_labels(graph, pos, labels=node_labels, font_size=8)
 
 # Show the plot
 plt.axis('off')
--- run.sh
+++ run.sh
@@ -1,8 +1,10 @@
 #! /bin/sh
 
+set -e
+
 pushd trust-network
 rm -f output.dot
 cargo run > ../output.dot
 popd
 
-python graph_layout.py
+python graph_layout.py output.dot
